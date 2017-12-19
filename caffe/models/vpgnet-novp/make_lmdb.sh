2e6b241692f1680cd4698fba9fae88c7c8244906
Seokju Lee <sjlee@rcv.kaist.ac.kr>
2017-12-19 15:41:07 +0900

    update caffe

--- caffe/models/vpgnet-novp/make_lmdb.sh
+++ caffe/models/vpgnet-novp/make_lmdb.sh
@@ -1,6 +1,6 @@
-../../build/tools/convert_driving_data /media/rcv/HDD/caltech-lanes /media/rcv/HDD/caltech-lanes/cordova1.txt LMDB_train
+../../build/tools/convert_driving_data $PATH_TO_DATASET_DIR$ train_list.txt LMDB_train
 ../../build/tools/compute_driving_mean LMDB_train ./driving_mean_train.binaryproto lmdb
 
-../../build/tools/convert_driving_data /media/rcv/HDD/caltech-lanes /media/rcv/HDD/caltech-lanes/cordova2.txt LMDB_test
+../../build/tools/convert_driving_data $PATH_TO_DATASET_DIR$ test_list.txt LMDB_test
 
 
